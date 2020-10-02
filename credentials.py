9ab42ac56d6bd9bb7f73a263b609bc376f6fe343
Blake Lambert <blambert1080@gmail.com>
2020-10-02 14:37:39 -0500

    add heroku config for deployment

--- credentials.py
+++ credentials.py
@@ -1,7 +1,9 @@
 import os
 
-_token = os.environ['BRO_BOT_TOKEN']
+_token      = os.environ['BRO_BOT_TOKEN']
+_league_id  = os.environ['BRO_LEAGUE_ID']
 
 login = {
-  'token': _token
+  'token': _token,
+  'league_id': _league_id
 }
--- espn_client.py
+++ espn_client.py
@@ -1,2 +1,7 @@
 from espn_api.football import League
+from credentials import login
 
+league = League(league_id=login['league_id'], year=2020)
+
+def get_draft():
+  draft = league.draft
