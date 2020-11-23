0caca6ac68be89b5b86e93845fbc5dd832c18898
Blake Lambert <blambert1080@gmail.com>
2020-11-23 11:56:09 -0600

    add draft status

--- discord_client.py
+++ discord_client.py
@@ -1,5 +1,6 @@
 import discord
 from credentials import login
+import espn_client
 
 TOKEN = login['token']
 client = discord.Client()
@@ -14,7 +15,20 @@ async def on_message(message):
   if message.content.startswith('!bro'):
     if 'blake' in message.content.lower():
       await message.channel.send(war_blake_gif)
+    if 'draft' in message.content.lower():
+      player_name = message.content.replace('!bro draft ', '')
+      player_info = espn_client.get_local_draft_info(player_name)
+      if player_info is "Undrafted":
+        await message.channel.send("This player was not found in the draft meaning that they were Undrafted or you spelled their name wrong.")
+        return
+      embed = make_embedded_draft_info(player_info)
+      await message.channel.send(embed=embed)
 
+def make_embedded_draft_info(player_info):
+    embed = discord.Embed(title="*{0}*".format(player_info["player"]))
+    embed.add_field(name="Round Drafted", value=player_info["round"], inline=False)
+    embed.add_field(name="Draft Pick", value=player_info["position"], inline=False)
+    return embed
 
 @client.event
 async def on_ready():
--- espn_client.py
+++ espn_client.py
@@ -1,7 +1,35 @@
 from espn_api.football import League
 from credentials import login
+import math
 
 league = League(league_id=login['league_id'], year=2020)
 
-def get_draft():
+def parse_local_draft_file():
+  draft_file = open('draft.txt', 'r')
+  draft_dict = {}
+  for line in draft_file:
+    player, team = line.strip().rsplit(',')
+    draft_dict[player.strip()] = team.strip()
+
+  draft_file.close()
+  return draft_dict
+
+
+__local_draft = parse_local_draft_file()
+
+
+def get_online_draft():
   draft = league.draft
+
+
+def get_local_draft_info(player):
+  if player in __local_draft:
+    draft_position = list(__local_draft).index(player) + 1
+    draft_round = 1 if (draft_position < 11) else (math.trunc(draft_position / 10) + 1)
+    return {
+      "player": player,
+      "round": draft_round,
+      "position": draft_position
+    }
+
+  return "Undrafted"
